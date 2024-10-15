62b2864f684051e6b5ed250189cb45f423020d5d
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 10:31:19 +0530

    Create Corresponds.py

---
+++ Corresponds.py
@@ -0,0 +1,15 @@
+'''
+Write a Program to find the search element in the given list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the elements of list in single line separated by space.
+Third input consists of the element which we need to search
+Sample Input 1:
+5
+1 2 3 6 5
+3
+'''
+3 is present in the given list
+
+
